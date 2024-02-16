e5ac1b1b41279990f6863d733f86be266d28f204
Idba1 <moniraislamself@gmail.com>
2024-02-16 11:14:12 +0600

    show play ground

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -3,7 +3,10 @@ function play (){
     // hide the home screen. to hide the screen add the class hidden to the home section
     const homeSection = document.getElementById('home-screen');
     homeSection.classList.add('hidden');
-    // console.log(homeSection.classList)
+    // console.log(homeSection.classList);
 
     // show the play ground
+    const playGround = document.getElementById('play-ground');
+    playGround.classList.remove('hidden');
+    // console.log(playGround.classList);
 }
