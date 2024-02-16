c779c1dcee99bd614a57880ecd1322f9faa09b38
Idba1 <moniraislamself@gmail.com>
2024-02-16 11:39:20 +0600

    show & hidden using by function

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,12 +1,17 @@
-function play (){
-    // console.log('play start now');
-    // hide the home screen. to hide the screen add the class hidden to the home section
-    const homeSection = document.getElementById('home-screen');
-    homeSection.classList.add('hidden');
-    // console.log(homeSection.classList);
+// function play (){
+//     // console.log('play start now');
+//     // hide the home screen. to hide the screen add the class hidden to the home section
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+//     // console.log(homeSection.classList);
 
-    // show the play ground
-    const playGround = document.getElementById('play-ground');
-    playGround.classList.remove('hidden');
-    // console.log(playGround.classList);
+//     // show the play ground
+//     const playGround = document.getElementById('play-ground');
+//     playGround.classList.remove('hidden');
+//     // console.log(playGround.classList);
+// }
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
 }
