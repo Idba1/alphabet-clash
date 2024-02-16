ebb8147e9395adcb2fce01036cd4a567378c09c5
Idba1 <moniraislamself@gmail.com>
2024-02-16 12:19:06 +0600

    show random alphabet in screen

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -17,6 +17,10 @@ function continueGame(){
     const alphabet = getARandomAlphabet();
     console.log("your random alphabet",alphabet)
 
+    // set randomly generated alphabet to the screen(show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
 }
 
 
